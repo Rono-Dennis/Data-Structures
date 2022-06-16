0cc2e172da60d07ecea7545b741ad33adca24584
Dennis <ronodennis580@gmail.com>
2022-06-16 21:50:34 +0300

    edits

--- src/ChechDuplicates.java
+++ src/ChechDuplicates.java
@@ -1,2 +1,26 @@
 public class ChechDuplicates {
-}
+    public static void main(String[] args) {
+            int [] nums = new int[0];
+            int max = 0;
+            if(nums.length<=1)
+            {
+//                return max;
+                System.out.println(max);
+            }
+            int[] dp = new int[nums.length];
+
+            for(int i=1; i<nums.length; i++){
+                for( int j=0; j<i; j++){
+                    if(nums[j] < nums[i]){
+                        dp[i] = Math.max(dp[i], dp[j]+1);
+                    }
+                }
+                max = Math.max(max, dp[i]);
+            }
+        System.out.println(max);
+//            return max;
+        }
+    }
+
+
+
