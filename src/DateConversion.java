fa0d00a82a27d93a374d539d57df7d09381a55e8
Dennis <ronodennis580@gmail.com>
2022-07-23 01:18:53 +0300

    edits

--- src/DateConversion.java
+++ src/DateConversion.java
@@ -95,9 +95,7 @@ class DateConversion {
 
     }
 
-}
 
-public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
