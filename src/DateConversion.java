84eea950a71b12a1be37003af10ad064eb81a625
Dennis <ronodennis580@gmail.com>
2022-07-23 01:16:16 +0300

    edits

--- src/DateConversion.java
+++ src/DateConversion.java
@@ -1,35 +1,115 @@
-// Java program to Find the Largest Alphabetic
-// Character present in the string of both
-// uppercase and lowercase English characters
+/**
+Given a time in
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+        -hour AM/PM format, convert it to military (24-hour) time.
+
+        Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+        - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+        Example
+
+        Return '12:01:00'.
+
+        Return '00:01:00'.
+
+        Function Description
+
+        Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+        timeConversion has the following parameter(s):
+
+        string s: a time in
+
+        hour format
+
+        Returns
+
+        string: the time in
+
+        hour format
+
+        Input Format
+
+        A single string
+        that represents a time in -hour clock format (i.e.: or
+
+        ).
+
+        Constraints
+
+        All input times are valid
+
+        Sample Input
+
+        07:05:45PM
+
+        Sample Output
+
+        19:05:45
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 class DateConversion {
 
-    public static void main(String[] args) {
-        Date dt = new Date();
-        SimpleDateFormat dateFormat;
-        dateFormat = new SimpleDateFormat("hh:mm:ss a");
-        System.out.println("Time in 12 hr format = "+dateFormat.format(dt));
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
 
-
-        Date date = new Date();
-        SimpleDateFormat twelveDateFormat;
-        twelveDateFormat = new SimpleDateFormat("kk:mm:ss");
-        System.out.println("Time in 24 hr format = "+twelveDateFormat.format(date));
-
-        if (dateFormat.format(dt) == "12:01:00 PM"){
-
-            twelveDateFormat.format(date);
-            return;
-        }
-        if (dateFormat.format(dt) == "12:01:00 AM")
+    public static String timeConversion(String s) {
+        // Write your code here
+        int s1 = Integer.valueOf(s.substring(0, 2));
+        int newInt = 0;
+        if(s1<12)
         {
-            twelveDateFormat.format(date);
-            return;
+            newInt = s1 + 12;
+            // s1 = 12 + 1;
+            // s1++;
         }
+        else if(s1>12){
+            newInt = s1 - 12;
+            // s1 = 00;
+            // s1++;
+        }
+        StringBuilder stringBuilder1 = new StringBuilder(s);
+        stringBuilder1.delete(0, 2);
+        String newTime = String.valueOf(newInt)+stringBuilder1.toString();
+        String newString = s1+s;
+        StringBuilder stringBuilder = new StringBuilder(newTime);
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        String toUse = stringBuilder.toString();
+        return toUse;
 
     }
+
 }
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = DateConversion.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- src/sqlQuery.java
+++ src/sqlQuery.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class sqlQuery {
 
     /*important sql queries*/
@@ -11,4 +13,18 @@ public class sqlQuery {
     UPDATE user SET image_url = CONCAT('https://randomuser.me/api/portraits/men/', FLOOR(RAND() * 100), '.jpg');
 
     SELECT FLOOR(RAND() * 100);*/
+
+
+
+    public static void main(String[] args) {
+
+        int [] arr = {1,2,3,4,5,7,8,10};
+        int min = 0;
+//        for(int i = 0; i<=arr.length;i++){
+            min = Arrays.stream(arr).filter(i -> i >= 0).min().orElse(0);
+//            System.out.println("min is "+ min);
+//        }
+        System.out.println("min is "+ min);
+    }
 }
+
